36f62632302d765d2564f7c0416a405dd7b8c8ea
Ronald Royales <rroyales@gmail.com>
2014-01-02 12:04:11 -0800

    pass protect

--- testme/settings.py
+++ testme/settings.py
@@ -61,7 +61,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'ronald',                      # Or path to database file if using sqlite3.
         'USER': 'ronald',                  # Not used with sqlite3.
-        'PASSWORD': 'aromaz3973',                # Not used with sqlite3.
+        'PASSWORD': 'changepassword',                # Not used with sqlite3.
         'HOST': 'localhost',                 # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
